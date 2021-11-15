ddd38a84c16ffaf7c7d58fe7f7aa77803f415d62
Andrew Cavalcante pacifico <andrew.pacifico@meliuz.com.br>
2021-11-14 20:23:50 -0400

    feat(core/logger): Add new logger module

---
+++ packages/core/lib/logger/logger.ts
@@ -0,0 +1,7 @@
+export interface ILogger {
+  init(): void;
+  info(msg: any, ...args: any[]): void;
+  debug(msg: any, ...args: any[]): void;
+  error(msg: any, ...args: any[]): void;
+  warn(msg: any, ...args: any[]): void;
+}
---
+++ packages/core/lib/logger/pino.ts
@@ -0,0 +1,44 @@
+import pinoModule, { P } from 'pino';
+
+import { ILogger } from './logger';
+
+type Pino = typeof pinoModule;
+
+type TDependencies = {
+  pino: Pino;
+};
+
+export default class PinoLogger implements ILogger {
+  public logger: P.Logger;
+
+  constructor(private deps: TDependencies) { }
+
+  private getLogger(): P.Logger {
+    if (!this.logger) {
+      throw new Error('Logger not initialized');
+    }
+
+    return this.logger;
+  }
+
+  init(): void {
+    const { pino } = this.deps;
+    this.logger = pino({});
+  }
+
+  info(msg: any, ...args: any[]): void {
+    this.getLogger().info(msg, ...args);
+  }
+
+  debug(msg: any, ...args: any[]): void {
+    this.getLogger().debug(msg, ...args);
+  }
+
+  warn(msg: any, ...args: any[]): void {
+    this.getLogger().warn(msg, ...args);
+  }
+
+  error(msg: any, ...args: any[]): void {
+    this.getLogger().error(msg, ...args);
+  }
+}
