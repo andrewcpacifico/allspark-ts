b34602dfacbdffbff7fbd4b7f5f51b06cff40be8
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2021-11-20 16:59:39 -0400

    refactor(builder/compile): Remove log

--- packages/builder/src/commands/compile.ts
+++ packages/builder/src/commands/compile.ts
@@ -3,7 +3,6 @@ import { spawnSync } from 'child_process';
 export const command = 'compile';
 export const desc = 'Compile typescript files from service';
 export function handler() {
-  console.log('VAI TOMÁ NO CU!!');
   spawnSync(
     './node_modules/.bin/tsc',
     ['-p', 'tsconfig.build.json'],
