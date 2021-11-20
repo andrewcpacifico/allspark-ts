c19c802ba27933eed470aa02f9f6bcf76db1efb9
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2021-11-20 17:25:06 -0400

    feat(builder/test-unit): Add test unit command

---
+++ packages/builder/src/commands/test-unit.ts
@@ -0,0 +1,29 @@
+import { spawnSync } from 'child_process';
+import { Argv } from 'yargs';
+
+const CONFIG_PATH = './node_modules/@allspark-js/builder/config/.mocharc.json';
+
+type Params = {
+  dev: boolean;
+};
+
+export const command = 'test-unit';
+export const desc = 'Run unit tests.';
+
+export function builder(yargs: Argv<Params>) {
+  return yargs.describe('dev', 'Watch modifications on development environment');
+}
+
+export function handler(argv: Params) {
+  const args = ['--config', CONFIG_PATH];
+
+  if (argv.dev) {
+    args.push('--watch');
+  }
+
+  spawnSync(
+    './node_modules/.bin/mocha',
+    args,
+    { stdio: 'inherit' },
+  );
+};
