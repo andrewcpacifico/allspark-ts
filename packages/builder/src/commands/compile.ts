f3e14d457b438a7d97e717ea2b80335b74d26d5e
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2021-11-21 13:20:29 -0400

    feat(builder): Add support to ts paths

--- packages/builder/src/commands/compile.ts
+++ packages/builder/src/commands/compile.ts
@@ -4,7 +4,7 @@ export const command = 'compile';
 export const desc = 'Compile typescript files from service';
 export function handler() {
   spawnSync(
-    './node_modules/.bin/tsc',
+    './node_modules/.bin/ttsc',
     ['-p', 'tsconfig.build.json'],
     { stdio: 'inherit' },
   );
