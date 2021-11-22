719718f4a193d8c3acfaefd4a667b131da537e42
Andrew Cavalcante pacifico <andrewcpacifico@gmail.com>
2021-11-22 08:30:59 -0400

    fix(rest): Fix moment import

--- packages/rest/src/third-party/index.ts
+++ packages/rest/src/third-party/index.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { Joi, validate } from 'express-validation';
-import moment from '@allspark-js/core/third-party';
+import moment from '@allspark-js/core/third-party/moment';
 
 export type ExpressModule = typeof express;
 export type ExpressValidator = {
